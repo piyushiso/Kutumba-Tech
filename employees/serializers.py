7dd66cf905c86ed717344e760e89d60e992b6079
Piyush L. Shrestha <75093611+piyushiso@users.noreply.github.com>
2025-03-13 00:04:21 +0545

    Delete employees directory

--- employees/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Employee
-
-# Converts complex model instances to further readable/useable data.
-
-class EmployeeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Employee
-        fields = '__all__'
